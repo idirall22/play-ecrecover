814a1d8fe04da72c2d9427e737bcfe36e41249ba
idir <idirall22@gmail.com>
2021-06-16 06:59:21 -0700

    update

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,29 @@
 use forest_crypto;
 use arrayref;
 use ethereum_hexutil as ex;
+use rust_crypto::digest::Digest;
+use rust_crypto::sha3::Sha3;
 
 fn main(){
     // binance block https://bscscan.com/block/8312800
     // the extra data signature
-    let extra_data = ex::read_hex("0x22c1c024a111a19c6e9d422ea1e390c3d6fffa63ba8648b3f763ed3a12812dc83d2b2fddf242b95506a6efc8e3237a9d42ad67d62d58fe93cb72583c1ad2322600").unwrap();
-    let sign = arrayref::array_ref!(extra_data[extra_data.len()-65..extra_data.len()], 0, 65);
+    let hash = ex::read_hex("0x73240ced7e0e5ef15925667d1deda0703d8e9a3fa384fb24239b21713bdb4eb3").unwrap();
+    let signature = ex::read_hex("0x22c1c024a111a19c6e9d422ea1e390c3d6fffa63ba8648b3f763ed3a12812dc83d2b2fddf242b95506a6efc8e3237a9d42ad67d62d58fe93cb72583c1ad2322600").unwrap();
     // block hash
-    let a = ex::read_hex("0x73240ced7e0e5ef15925667d1deda0703d8e9a3fa384fb24239b21713bdb4eb3").unwrap();
-    let h= arrayref::array_ref![a, 0, 32];
+    let h = arrayref::array_ref!(hash, 0, 32);
+    let s = arrayref::array_ref!(signature, 0, 65);
     // recover pub key
-    let signer = forest_crypto::signature::ecrecover(h, sign);
-    let r = hex::encode(&signer.unwrap().payload().to_raw_bytes());
+    let signer = forest_crypto::signature::ecrecover(h, s);
+    let r = &signer.unwrap().payload().to_raw_bytes();
+    // println!("{:?}", r);
     // print
-    println!("{:?}", r);
+    let mut hasher = Sha3::keccak256();
+    hasher.input(&r[1..r.len()]);
+    let out = hasher.result_str();
+    // // let out = hasher.result_str();
+    // // // copy(signer[:], crypto.Keccak256(pubkey[1:])[12:])
+    // // let xout = ex::read_hex(&out).unwrap();
+ 
+    // // 0xb218c5d6af1f979ac42bc68d98a5a0d796c6ab01
+    println!("{:?}", out );
 }
